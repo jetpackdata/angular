f33d02ed8437bc91b43fe9bf5945a9b6fc67b214
Shankar Arul <shankar.vinay.arul@gmail.com>
2014-05-27 01:17:39 +0200

    completed filters

--- js/app.js
+++ js/app.js
@@ -123,3 +123,15 @@ app.directive("orange",function(){
     }
   };
 });
+
+app.filter("exclude",function(){
+  return function(input,exclude){
+    var result=[];
+    for (var i=0;i<input.length;i++){
+      if (input[i]!=exclude)
+        result.push(input[i]);
+    }
+
+    return result;
+  };
+});
